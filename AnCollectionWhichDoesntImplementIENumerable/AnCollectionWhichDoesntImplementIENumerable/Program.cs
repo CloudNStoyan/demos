1787ff3686bb40af7955473904d9549f27e3d6e6
Stoyan <slownessbg@gmail.com>
2019-03-04 13:34:27 +0200

    init commit

---
+++ AnCollectionWhichDoesntImplementIENumerable/AnCollectionWhichDoesntImplementIENumerable/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace AnCollectionWhichDoesntImplementIENumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClasses = new Film[]
+            {
+                new Film("Jackie Chan's bizzare adventure"), new Film("Stargate The Film"), new Film("Men In Black 1"), 
+            };
+
+            var cinema = new Cinema(myClasses);
+
+
+            foreach (var film in cinema)
+            {
+                Console.WriteLine(film.Title);
+            }
+        }
+    }
+
+    class Film
+    {
+        public string Title { get; }
+
+        public Film(string title)
+        {
+            this.Title = title;
+        }
+    }
+
+    class Cinema
+    {
+        private Film[] Collection { get; set; }
+
+        public Cinema(Film[] myArray)
+        {
+            this.Collection = myArray;
+        }
+
+        public CinemaEnum GetEnumerator()
+        {
+            return new CinemaEnum(this.Collection);
+        }
+    }
+
+    class CinemaEnum
+    {
+        private int position = -1;
+
+        private Film[] Collection { get; }
+
+        public CinemaEnum(Film[] list)
+        {
+            this.Collection = list;
+        }
+
+        public Film Current
+        {
+            get { return this.Collection[this.position]; }
+        }
+
+
+        public bool MoveNext()
+        {
+            this.position++;
+            return (this.position < this.Collection.Length);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Demos for stuff i learn.
+This repository will contain demos for sujbects i learn.
