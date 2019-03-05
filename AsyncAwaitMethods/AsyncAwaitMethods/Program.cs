cdffa77ea63707c104619e9847af07f9d7cd3c92
Stoyan <slownessbg@gmail.com>
2019-03-05 20:36:39 +0200

    Adding async await demo

---
+++ AsyncAwaitMethods/AsyncAwaitMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Open file(Path): ");
+            string path = Console.ReadLine();
+
+            var result = ReadFile(path);
+            Console.WriteLine("Say something to see that the thread is not blocked: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("You said: " + a);
+
+            result.Wait();
+            Console.WriteLine(result.Result);
+        }
+
+        static async Task<string> ReadFile(string path)
+        {
+            using (var reader = File.OpenText(path))
+            {
+                var file = await reader.ReadToEndAsync();
+
+
+                for (int i = 0; i < 1000000; i++)
+                {
+                    var a = DateTime.Now.ToString();
+                }
+
+                Console.WriteLine("I finished!");
+
+                return file;
+            }
+        }
+    }
+}
