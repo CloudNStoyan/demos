9be2444cc4088dded5aa778315b5db0313391638
Stoyan <slownessbg@gmail.com>
2019-03-09 22:13:47 +0200

    Trying stuff with encryption

---
+++ EncryptingAndDecrypting/EncryptingAndDecrypting/Cryptoclass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptingAndDecrypting
+{
+    public static class Cryptoclass
+    {
+        public static string Encrypt(string text, string key)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            alphabet += alphabet.ToUpperInvariant();
+            string reversealphabet = new string(alphabet.Reverse().ToArray());
+
+            var encryptedBuilder = new StringBuilder();
+
+            int keyCount = 0;
+            foreach (var letter in text)
+            {
+                char letterOfKey = key[keyCount];
+
+                int indexOf = text.IndexOf(letter);
+
+
+                int index = letter + indexOf;
+                index += letterOfKey;
+                
+                index -= reversealphabet.IndexOf(letterOfKey);
+                encryptedBuilder.Append((char)index);
+
+                if ((keyCount + 1) < (key.Length - 1))
+                {
+                    keyCount++;
+                }
+                else
+                {
+                    keyCount = 0;
+                }
+            }
+
+            return encryptedBuilder.ToString();
+        }
+
+        public static string Decrypt(string text, string key)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            alphabet += alphabet.ToUpperInvariant();
+            string reversealphabet = new string(alphabet.Reverse().ToArray());
+
+            var decryptedBuilder = new StringBuilder();
+
+            int keyCount = 0;
+            foreach (var letter in text)
+            {
+                char letterOfKey = key[keyCount];
+
+                int indexOf = text.IndexOf(letter);
+
+
+                int index = letter + indexOf;
+                index -= letterOfKey;
+
+                index -= reversealphabet.IndexOf(letterOfKey);
+                Console.WriteLine(index);
+                decryptedBuilder.Append((char)index);
+
+                if ((keyCount + 1) < (key.Length - 1))
+                {
+                    keyCount++;
+                }
+                else
+                {
+                    keyCount = 0;
+                }
+            }
+
+            return decryptedBuilder.ToString();
+        }
+    }
+}
---
+++ EncryptingAndDecrypting/EncryptingAndDecrypting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptingAndDecrypting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Cryptoclass.Encrypt("This is message", "stoyan");
+            string b = Cryptoclass.Decrypt(a, "stoyan");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
