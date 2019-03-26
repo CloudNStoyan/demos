8729115999c4c0aa4f6c232bd7fcd750847e18c4
Stoyan <slownessbg@gmail.com>
2019-03-26 23:06:16 +0200

    Adding new demo Logging

---
+++ TryingLogging/TryingLogging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace TryingLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Logger = logger;
+
+            Log.Information("Added user.");
+
+            Console.WriteLine("This shows up later in the code so the real log is in the time being created.");
+        }
+    }
+}
