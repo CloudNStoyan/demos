6023e94caa0ea386dfb235f0eeac4cd9be2103f1
Stoyan <slownessbg@gmail.com>
2019-08-20 23:58:32 +0300

    Adding subviewer regex

--- SubtitleManager/SubtitleManager/CustomStuff/RegexMatches.cs
+++ SubtitleManager/SubtitleManager/CustomStuff/RegexMatches.cs
@@ -3,5 +3,7 @@
     public class RegexMatches
     {
         public const string SubRipMatchRegex = @"(?<Order>\d+)\r\n(?<StartTime>(\d\d:){2}\d\d,\d{3}) --> (?<EndTime>(\d\d:){2}\d\d,\d{3})\r\n(?<Sub>.+)(?=\r\n\r\n\d+|$)";
+
+        public const string SubViewerMatchRegex = @"((?<StartTime>(\d\d:\d\d:\d\d.\d\d),(?<EndTime>(\d\d:\d\d:\d\d.\d\d))))";
     }
 }
